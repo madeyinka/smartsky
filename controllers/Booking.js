78bd29d55d054695fb9433b88288a1496ec627fc
madeyinka007 <mutairadeyinka@yahoo.com>
2021-09-12 23:23:14 +0100

    Date Update

--- controllers/Booking.js
+++ controllers/Booking.js
@@ -6,8 +6,8 @@ const initBooking = {
 
     make_booking: (param, callback) => { //check if userid is passed
         const id = "#QUO-" + Util.rand_str(8, '1234567890')
-        const data = {quote_id: id, origin:param.origin, destination:param.destination, shipment_date:param.shipment_date,
-        submission:param.submission, location:param.location, recipient:param.recipient, cargoes:param.cargoes, total_cost:param.total_cost,
+        const data = {quote_id:id, origin:param.origin, destination:param.destination, shipment_date:param.shipment_date,
+        submission:param.submission,location:param.location,recipient:param.recipient,cargoes:param.cargoes,total_cost:param.total_cost,
         unit:param.unit,dim_weight:param.dim_weight,actual_weight:param.actual_weight,chargable_weight:param.chargable_weight,status:param.status}
         bookModel.save(data, (resp) => {
             console.log(resp)
