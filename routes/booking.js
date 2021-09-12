0dfbf1b04cc06b0c9c57934d718d941b480f15a9
madeyinka007 <mutairadeyinka@yahoo.com>
2021-09-12 23:55:16 +0100

    Authenticate User

--- routes/booking.js
+++ routes/booking.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const Util = require('./../libraries/Utility')
 const Booking = require('./../controllers/Booking')
+const authenticate = require('./../middleware/Authenticate')
 
-router.post('/create', (req, res) => {
+router.post('/create', authenticate, (req, res) => {
     Booking.make_booking(Util.param_extract(req), (state) => {
         Util.resp(res).json(state)
     })
